a9549e948601635cf0bf5c0e6d279736d31d4313
manvishah27 <manvi.shah@somaiya.edu>
2019-10-12 00:19:29 +0530

    Components part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   imports:      
@@ -16,9 +18,10 @@ import 'hammerjs';
     FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule 
+    FlexLayoutModule,
+    MatListModule
   ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, MenuComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
